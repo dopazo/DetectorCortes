f61941ea5b355b3656bd4d0b0a2b57434317c781
dopazo <30671074+dopazo@users.noreply.github.com>
2018-11-06 23:31:14 -0300

    conectado a firebase
    La aplicación se conecta a la base de datos firebase, logra escribir datos y leerlos. Actualmente solo los lee mostrandolos por pantalla en una notificación Toast.

--- app/src/main/java/com/example/diego/DetectorCortes/MainActivity.kt
+++ app/src/main/java/com/example/diego/DetectorCortes/MainActivity.kt
@@ -4,9 +4,16 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.View
 import android.widget.*
+import com.google.firebase.FirebaseError
 import kotlinx.android.synthetic.main.activity_main.*
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity(), TextWatcher, View.OnClickListener {
 
@@ -14,16 +21,23 @@ class MainActivity : AppCompatActivity(), TextWatcher, View.OnClickListener {
     //terminar el tipo en "?" para permitir que este vacia o null
 
     private var editLugar: EditText? = null
-    private var editTelefono: EditText? = null
+    private var editNumero: EditText? = null
     private var button: Button? = null
     private var lugar = ""
-    private var telefono = ""
+    private var Numero = ""
     internal var lista: ListView? = null
     private var num = 10
     private var count = 1
     internal var lugarLv: Array<String>? = null
-    internal var telefonoLv: Array<String>? = null
+    internal var NumeroLv: Array<String>? = null
 
+    //conectar a firebase
+    var database = FirebaseDatabase.getInstance()
+    var myRef = database.getReference("Devices")
+
+    companion object {
+        val TAG = "ChatLog"
+    }
     //metodo que se ejecuta al iniciar la aplicacion
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,19 +47,36 @@ class MainActivity : AppCompatActivity(), TextWatcher, View.OnClickListener {
         //"R." para buscar en la pantalla según tengo entendido
         //"!!" significa que debe tener un valor obligatoriamente (no puede estar null)
         editLugar = findViewById(R.id.editText_Lugar) as EditText
-        editTelefono = findViewById(R.id.editText_Telefono) as EditText
+        editNumero = findViewById(R.id.editText_Numero) as EditText
         button = findViewById(R.id.button_Agregar) as Button
         button!!.setOnClickListener(this)
         editLugar?.addTextChangedListener(this)
-        editTelefono?.addTextChangedListener(this)
+        editNumero?.addTextChangedListener(this)
         lista?.findViewById<ListView>(R.id.listaLugares)
 
-        lugarLv = Array<String>(10, {""})
-        telefonoLv = Array<String>(10, {""})
+        lugarLv = Array<String>(20, {""})
+        NumeroLv = Array<String>(20, {""})
+
+
+        //READ DATA FROM FIREBASE
+        val readPath = myRef.child("probando")
+        readPath.addValueEventListener(object : ValueEventListener{
+            override fun onCancelled(p0: DatabaseError?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onDataChange(snapshot: DataSnapshot?) {
+                val children = snapshot!!.children
+                children.forEach {
+                    println(it.toString())
+                    val dataReaded = it.toString()
+                    Toast.makeText(applicationContext , dataReaded, Toast.LENGTH_SHORT).show()
+                }
+            }
+        })
+
 
         //addTextChangedListener(this) <- muestra mini notificacion
-
-        //operacion()
     }
 
     //interface que se ejecuta despues de que ingresamos algo al campo de texto
@@ -68,6 +99,7 @@ class MainActivity : AppCompatActivity(), TextWatcher, View.OnClickListener {
         //Toast.makeText(this, s.toString(), Toast.LENGTH_SHORT).show()
     }
 
+    //AL CLICKEAR EL BOTON AGREGAR
     override fun onClick(v: View?) {
         when(v!!.id) {
             R.id.button_Agregar -> operacion()
@@ -76,23 +108,40 @@ class MainActivity : AppCompatActivity(), TextWatcher, View.OnClickListener {
     private fun operacion(){
         //si hay dato vacio, se hace "focus" para indicar que lo llenee
         lugar = editLugar?.text.toString()
-        telefono = editTelefono?.text.toString()
+        Numero = editNumero?.text.toString()
         if(lugar?.equals("") ?: ("" === null)){
             editLugar!!.requestFocus()
         }else{
-            if (telefono?.equals("") ?: ("" === null)){
-                editTelefono!!.requestFocus()
+            if (Numero?.equals("") ?: ("" === null)){
+                editNumero!!.requestFocus()
             }else{
                 var lugares: Array<String>
                 for (i in 0..num){
-                    //evitar crasheo al ingresar 10 lugares
+                    //evitar crasheo al ingresar 20 lugares
                     if(count == num+1){
                         Toast.makeText(this, "Cantidad máxima alcanzada", Toast.LENGTH_SHORT).show()
                         break
                     }
                     if(lugarLv?.get(i).equals("")){
+
+                        //subir a firebase
+
+                        val textLugar = editText_Lugar.text.toString()
+                        val textNumero = editText_Numero.text.toString()
+                        val key = myRef.push().key
+                        Log.d(TAG, textLugar)
+                        Log.d(TAG, textNumero)
+                        Log.d(TAG, key)
+
+                        myRef!!.child(key.toString()).child("Lugar").setValue(textLugar)
+                        myRef!!.child(key.toString()).child("Numero").setValue(textNumero)
+                        myRef!!.child(key.toString()).child("Estado_Corte_Energia").setValue("ON")
+                        myRef!!.child(key.toString()).child("Timestamp").setValue(System.currentTimeMillis())
+                        //timrstamp system.currentmillis
+                        Toast.makeText(this, "intentando subir a firebase", Toast.LENGTH_SHORT).show()
+
                         lugarLv?.set(i, lugar)
-                        telefonoLv?.set(i, telefono)
+                        NumeroLv?.set(i, Numero)
                         lugares = Array(count,{""})
                         for (j in 0..i){
                             lugares[j] = lugarLv?.get(j) as String
